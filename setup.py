fe2fe2dd4836895d58b950586fd40d208c72967f
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-03-22 11:24:15 +0100

    added the basic classes for scrapy

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='simurg',
+    version='0.0.1',
+    description='A Dataset for Training and Testing Abstractive Summarizers',
+    url='http://dbs.cs.uni-duesseldorf.de',
+    author='Pashutan Modaresi',
+    author_email='modaresi@cs.uni-duesseldorf.de',
+    packages=find_packages(exclude=['tests']),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=[]
+)
