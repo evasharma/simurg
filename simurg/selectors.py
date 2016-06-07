601662022de3ddb3c143c427bce792d4fc6dd865
Pashutan Modaresi <pashutan.modaresi@pressrelations.de>
2016-06-07 14:00:24 +0200

    added selectors.py to contain default css selectors

---
+++ simurg/selectors.py
@@ -0,0 +1,11 @@
+# Main page consists of 10 top stories
+TOP_STORY_LINK_SELECTOR = 'div.topic a'
+
+# Each top story consists of news sections
+NEWS_ELEMENT_SELECTOR = 'div.blended-wrapper.esc-wrapper'
+
+# Each news section contains a link to the news
+NEWS_LINK_SELECTOR = 'h2.esc-lead-article-title a'
+
+# Each news section contains the headline of the news
+HEADLINE_SELECTOR = 'span.titletext'
